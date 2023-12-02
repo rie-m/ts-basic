4f9c4f01dae2ab44864653ed43a163ab7df70d3d
Rie Matsufusa <rie.matsufusa@gmail.com>
2023-12-02 15:06:58 +0900

    tsbasic #06

---
+++ src/array/tuple.ts
@@ -0,0 +1,13 @@
+export default function tupleSample() {
+    // 一般的なタプルの型定義
+    let response: [number, string] = [200, 'OK']
+    // response = [400, 'NG', 'too much']
+    // response = ['not number', 'NG']
+    console.log('Array tuple sample 1:', response)
+
+    // 可変長引数を使ったタプル
+    const girlfriends: [string, ...string[]] = ['Kana', 'Miku']
+    girlfriends.push('Misa')
+    console.log('Array tuple sample 2:', girlfriends)
+
+}
