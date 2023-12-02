dbe840257396dd273ba066bc4a5ee624a92becad
Rie Matsufusa <rie.matsufusa@gmail.com>
2023-12-02 13:53:17 +0900

    tsbasic #05

--- src/object/object.ts
+++ src/object/object.ts
@@ -1,55 +1,54 @@
 export default function objectSample() {
-    const a: object = {
-        name: 'rie',
-        age
-        :28
-    }
-    // a.name
+  // const a: object = {
+  //     name: 'rie',
+  //     age :28
+  // }
+  // a.name
 
-    // オブジェクトリテラル記法で型定義
-    let country: {
-        language: string
-        name: string
-    } = {
-        language: 'Japanese',
-        name: 'Japan'
-    }
+  // オブジェクトリテラル記法で型定義
+  let country: {
+    language: string
+    name: string
+  } = {
+    language: 'Japanese',
+    name: 'Japan',
+  }
 
-    console.log('Object object sample 1:', country)
+  console.log('Object object sample 1:', country)
 
-    country = {
-        language: 'English',
-        name: 'United States of America'
-    }
-    console.log('Object object sample 2:', country)
+  country = {
+    language: 'English',
+    name: 'United States of America',
+  }
+  console.log('Object object sample 2:', country)
 
-    // オプショナルとreadonly
-    const torahack: {
-        age: number
-        lastName: string
-        readonly firstName: string
-        gender?: string
-    } = {
-        age: 28,
-        lastName: 'Yamada',
-        firstName: 'Tarou'
-    }
+  // オプショナルとreadonly
+  const torahack: {
+    age: number
+    lastName: string
+    readonly firstName: string
+    gender?: string
+  } = {
+    age: 28,
+    lastName: 'Yamada',
+    firstName: 'Tarou',
+  }
 
-    torahack.lastName = 'Satou'
-    // torahack.firstName = 'Wataru'
-    torahack.gender = 'male'
-    console.log('Object object sample 3:', torahack)
+  torahack.lastName = 'Satou'
+  // torahack.firstName = 'Wataru'
+  torahack.gender = 'male'
+  console.log('Object object sample 3:', torahack)
 
-    // インデックスシグネチャ
-    // オブジェクトが複数のプロパティを持つ可能性を示す
-    const capitals: {
-        [countryName: string]: string
-    } = {
-        Japan: 'Tokyo',
-        Korea: 'Seoul'
-    }
+  // インデックスシグネチャ
+  // オブジェクトが複数のプロパティを持つ可能性を示す
+  const capitals: {
+    [countryName: string]: string
+  } = {
+    Japan: 'Tokyo',
+    Korea: 'Seoul',
+  }
 
-    capitals.China = 'Beijing'
-    capitals.Canada = 'Ottawa'
-    console.log('Object object sample 4:', capitals)
+  capitals.China = 'Beijing'
+  capitals.Canada = 'Ottawa'
+  console.log('Object object sample 4:', capitals)
 }
