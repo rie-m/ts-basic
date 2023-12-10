ee2536d5c96868769a9727881f5b571f5a4c6df1
Rie Matsufusa <rie.matsufusa@gmail.com>
2023-12-10 20:19:45 +0900

    tsbasic #09

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,15 @@
 // tupleSample()
 
 // 07. ジェネリック型とポリモーフィズム
-import genericsBasicSample from './generics/basic'
-import genericAdvancedSample from './generics/advanced'
-genericsBasicSample()
-genericAdvancedSample()
+// import genericsBasicSample from './generics/basic'
+// import genericAdvancedSample from './generics/advanced'
+// genericsBasicSample()
+// genericAdvancedSample()
+
+//10. 非同期処理
+import callbackSample from "./asynchronous/callback";
+import promiseSample from "./asynchronous/promise";
+import asyncAwaitSample from "./asynchronous/asyncAwait";
+callbackSample()
+promiseSample()
+asyncAwaitSample()
